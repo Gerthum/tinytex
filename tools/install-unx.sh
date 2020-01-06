531167a32bca4de73c151777d16f56f8ce333a7c
Gerthum <gerthum@gmail.com>
2020-01-07 10:03:13 +1300

    Change custom package location

--- tools/install-unx.sh
+++ tools/install-unx.sh
@@ -17,7 +17,7 @@ mkdir -p $TEXDIR
 mv texlive/* $TEXDIR
 rm -r texlive
 
-$TEXDIR/bin/*/tlmgr install $(download https://yihui.org/gh/tinytex/tools/pkgs-custom.txt | tr '\n' ' ')
+$TEXDIR/bin/*/tlmgr install $(download https://raw.githubusercontent.com/Gerthum/tinytex/master/tools/pkgs-custom.txt | tr '\n' ' ')
 
 if [ "$1" = '--admin' ]; then
   if [ "$2" != '--no-path' ]; then
